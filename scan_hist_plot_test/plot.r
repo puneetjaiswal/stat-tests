52596c638a5a88c93762f217841c118d3c1799b1
Puneet Jaiswal <punit.kj@gmail.com>
2017-02-06 19:02:41 -0800

    R test to scan, mean, histogram, stem and boxplot

---
+++ scan_hist_plot_test/plot.r
@@ -0,0 +1,15 @@
+fluo.conc = scan("/opt/Code/My/R-Tests/scan_hist_plot_test/Fluoride.txt")
+
+mean(fluo.conc)
+
+# histogram with multiple options
+hist(fluo.conc)
+hist(fluo.conc, main = "Histogram for Fluoride Concentrations", xlab = "concentration mg/g", ylab = "Freq", border = "red", col = "blue“, breaks = 10)
+
+# making stem
+stem(fluo.conc)
+
+# boxplot
+boxplot(fluo.conc, main ="Box Plot for Fluoride Concentrations", ylab="Concentration mg/g")
+
+
